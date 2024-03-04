02117e4149dd707f38cfe95a60058d531be98f22
Asim Maredia <amaredia3@gmail.com>
2024-03-03 16:30:56 -0800

    successfully compiles

--- trader.py
+++ trader.py
@@ -5,7 +5,7 @@ import string
 class Trader:
     
     def run(self, state: TradingState):
-				"""
+        """
         Only method required. It takes all buy and sell orders for all symbols as an input,
         and outputs a list of orders to be sent
         """
